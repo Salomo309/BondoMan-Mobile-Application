93cc395d6cb01f054d65108efc36b6fef574c1cf
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2024-03-16 12:17:50 +0700

    refactor: fix typo

--- app/src/main/java/com/example/bondoman/MainActivity.kt
+++ app/src/main/java/com/example/bondoman/MainActivity.kt
@@ -39,6 +39,8 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
     public fun disableNavBar() {
         for (i in 0 until binding.navView.menu.size()) {
             val menuItem = binding.navView.menu.getItem(i)
