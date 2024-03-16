9f879c5a5d0ec25f099874cdd13fa7986ca7c4c2
mikeleo03 <leonmichael463@gmail.com>
2024-03-16 11:41:27 +0700

    refactor : base repository and retrofit instance update

--- app/src/main/java/com/example/bondoman/repository/Repository.kt
+++ app/src/main/java/com/example/bondoman/repository/Repository.kt
@@ -1,23 +1,21 @@
 package com.example.bondoman.repository
 
-import android.content.Context
-import android.widget.Toast
 import com.example.bondoman.models.AuthRequest
 import com.example.bondoman.models.ItemsList
-import com.example.bondoman.models.UploadResponse
 import com.example.bondoman.service.RetrofitClient
-import com.example.bondoman.storage.TokenManager
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.toRequestBody
 
 class Repository {
+    // Doing Login
     suspend fun login(email: String, password: String): String {
         val loginRequest = AuthRequest(email, password)
         val response = RetrofitClient.authService.login(loginRequest)
         return response.token
     }
 
+    // Upload image captured
     suspend fun upload(file: ByteArray, token: String): ItemsList {
         val fileBody = file.toRequestBody("image/*".toMediaTypeOrNull())
         val response = RetrofitClient.fileService.upload("Bearer $token",
--- app/src/main/java/com/example/bondoman/service/RetrofitInstance.kt
+++ app/src/main/java/com/example/bondoman/service/RetrofitInstance.kt
@@ -7,19 +7,18 @@ object RetrofitClient {
 
     private const val BASE_URL = "https://pbd-backend-2024.vercel.app"
 
-    val authService: AuthService by lazy {
+    private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(AuthService::class.java)
+    }
+
+    val authService: AuthService by lazy {
+        retrofit.create(AuthService::class.java)
     }
 
     val fileService: FileService by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(FileService::class.java)
+        retrofit.create(FileService::class.java)
     }
 }
