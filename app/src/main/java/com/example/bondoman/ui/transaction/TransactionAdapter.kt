b2d1e6f372151dfed0ed146d54353d612fee99ea
mikeleo03 <leonmichael463@gmail.com>
2024-03-20 22:33:29 +0700

    feat : updated adapter of list transaction and some ui

--- app/src/main/java/com/example/bondoman/ui/transaction/TransactionAdapter.kt
+++ app/src/main/java/com/example/bondoman/ui/transaction/TransactionAdapter.kt
@@ -5,6 +5,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
 import com.example.bondoman.R
 import com.example.bondoman.models.Transaction
 
@@ -12,9 +15,11 @@ class TransactionAdapter(var transactions: List<Transaction>) : RecyclerView.Ada
     inner class TransactionViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         val title: TextView = itemView.findViewById(R.id.title)
         val date: TextView = itemView.findViewById(R.id.date)
+        val month: TextView = itemView.findViewById(R.id.month)
+        val time: TextView = itemView.findViewById(R.id.time)
         val amount: TextView = itemView.findViewById(R.id.amount)
         val category: TextView = itemView.findViewById(R.id.category)
-        val location: TextView = itemView.findViewById(R.id.location)
+        // val location: TextView = itemView.findViewById(R.id.location)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
@@ -25,10 +30,26 @@ class TransactionAdapter(var transactions: List<Transaction>) : RecyclerView.Ada
     override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
         val currentTransaction = transactions[position]
         holder.title.text = currentTransaction.title
-        holder.date.text = currentTransaction.date.toString()
-        holder.amount.text = currentTransaction.amount.toString()
+
+        // Extracting day, month, and time
+        val dateFormat = SimpleDateFormat("dd", Locale.getDefault())
+        val monthFormat = SimpleDateFormat("MMM", Locale.getDefault())
+        val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+        val date = dateFormat.format(currentTransaction.date)
+        val month = monthFormat.format(currentTransaction.date).toLowerCase(Locale.getDefault())
+        val time = timeFormat.format(currentTransaction.date)
+
+        holder.date.text = date
+        holder.month.text = month
+        holder.time.text = time
+
+        // Converting amount to Indonesian currency (Rupiah)
+        val amountFormat = NumberFormat.getCurrencyInstance(Locale("id", "ID"))
+        val formattedAmount = amountFormat.format(currentTransaction.amount)
+        holder.amount.text = formattedAmount
+
         holder.category.text = currentTransaction.category
-        holder.location.text = currentTransaction.location
+        // holder.location.text = currentTransaction.location
     }
 
     override fun getItemCount(): Int {
